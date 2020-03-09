645aba114629d7b3031d60087cab4ce2d17498cf
MagicPizzaBox <reeseh099@gmail.com>
2020-03-08 23:18:28 -0500

    Added previous progress

---
+++ title.js
@@ -0,0 +1,15 @@
+var titleString = "";
+var i = 0;
+var arr = ["R","e","e","s","e"," ","H","."];
+function update() {
+    titleString += arr[i];
+    i++;
+    document.getElementById('hola').innerHTML = titleString;
+    if (!(i < arr.length)) {
+        $(document).ready(function(){
+            $("#subtitle").fadeIn(1500);
+        });
+        clearInterval(myVar);
+    }
+}
+var myVar = setInterval(update, 100);
