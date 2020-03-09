9f5efc0eec5445c5facde1ed04a91f36505dea46
MagicPizzaBox <reeseh099@gmail.com>
2020-03-09 10:45:27 -0500

    fixed animations occuring before done loading

--- title.js
+++ title.js
@@ -1,15 +1,15 @@
-var titleString = "";
-var i = 0;
-var arr = ["R","e","e","s","e"," ","H","."];
-function update() {
-    titleString += arr[i];
-    i++;
-    document.getElementById('hola').innerHTML = titleString;
-    if (!(i < arr.length)) {
-        $(document).ready(function(){
-            $("#subtitle").fadeIn(1500);
-        });
-        clearInterval(myVar);
+$(document).ready(function(){
+    var titleString = "";
+    var i = 0;
+    var arr = ["R","e","e","s","e"," ","H","."];
+    function update() {
+        titleString += arr[i];
+        i++;
+        document.getElementById('hola').innerHTML = titleString;
+        if (!(i < arr.length)) {
+                $("#subtitle").fadeIn(1500);
+            clearInterval(myVar);
+        }
     }
-}
-var myVar = setInterval(update, 100);
+    var myVar = setInterval(update, 100);
+});
