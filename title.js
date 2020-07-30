75f76689af82d5bac8987f3cf393a900e67e876a
MagicPizzaBox <reeseh099@gmail.com>
2020-07-29 22:08:12 -0500

    reverted to original web

---
+++ title.js
@@ -0,0 +1,27 @@
+/*$(document).ready(function(){
+    var titleString = "";
+    var i = 0;
+    var arr = ["R","e","e","s","e"," ","H","."];
+    function update() {
+        titleString += arr[i];
+        i++;
+        document.getElementById('hola').innerHTML = titleString;
+        if (!(i < arr.length)) {
+                $("#subtitle").fadeIn(1500);
+            clearInterval(myVar);
+        }
+    }
+    var myVar = setInterval(update, 100);
+});*/
+$(document).ready(function(){
+    $("#hola").animate({bottom: '30px', opacity: '1'}, 3000);
+    var i = 0;
+    function update() {
+        if (i > 0) {
+            $("#subtitle").animate({bottom: '30px', opacity: '1'}, 1500);
+            clearInterval(myVar);
+        }
+        i++;
+    }
+    var myVar = setInterval(update, 500);
+});
